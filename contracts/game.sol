f8ed4bfc088d601c17552c523959bd5077b6b367
jacopo1498 <jacopo1498@gmail.com>
2024-06-19 11:53:26 +0200

    added input check

--- contracts/game.sol
+++ contracts/game.sol
@@ -29,7 +29,7 @@ contract game {
         uint cMaster; //who is currently the codemaster? 0 creator 1 challenger, or maybe bool is safer
         bool afk_CHECK; //true = set false =unset
         Guess[] movesHistory;
-        Feedback[] feedbackHistory;
+        Feedback[] feedbackHistory;//in theory the link between movesHistory and feedback History is implicit...depend on n of feedback and position (ex. mH[1]=fH[1])
         //to fill
     }
 
@@ -107,14 +107,54 @@ contract game {
         emit DepositHashSolution(msg.sender,uploadedHash,matchID);
     }
 
+    //true: all ok false:problem
+    function checkinput(Guess memory newGuess) private pure returns(bool){
+        //all guessable colors go from 1 to 6 (6 colors in 4 positions)
+        if (newGuess.pos1 > 0 && newGuess.pos1 < 7 &&
+            newGuess.pos2 > 0 && newGuess.pos2 < 7 &&
+            newGuess.pos3 > 0 && newGuess.pos3 < 7 &&
+            newGuess.pos4 > 0 && newGuess.pos4 < 7){
+                return true;
+            }else{
+                return false;
+            }
+    }
+
+    //true: all ok false:problem
+    function checkFeeback(Feedback memory newFeedback) private pure returns(bool){
+        //all guessable colors go from 1 to 6 (6 colors in 4 positions)
+        if ((newFeedback.wrong_pos < 0 || newFeedback.wrong_pos > 4) || 
+            (newFeedback.correct < 0 || newFeedback.correct > 4) || 
+            ((newFeedback.correct + newFeedback.wrong_pos) > 4)){
+                return false;
+            }else{
+                return true;
+            }
+    }
+
     function makeGuess(uint matchID, Guess memory uploadedGuess) public onlyCodeBreaker(matchID){
         //uncheck the afk flag 
         actOnAfkFlag(matchID,false);//true is set, false is unset
+        
+        //checkinput
+        require(checkinput(uploadedGuess),"incorrectly set guess, ivalid colors");
+
         //upload the guess in the array of moves
         for (uint i = 0; i < masterMind.length; i++) {
             if (masterMind[i].matchID == matchID) {
-                masterMind[i].movesHistory.push(uploadedGuess); 
-                emit DepositGuess(msg.sender,uploadedGuess,matchID);
+                //must check that i didn't already made a guess that is waiting for an answer
+                if (masterMind[i].movesHistory.length == masterMind[i].feedbackHistory.length){
+
+                    masterMind[i].movesHistory.push(uploadedGuess); 
+                    emit DepositGuess(msg.sender,uploadedGuess,matchID);
+                }else if (masterMind[i].movesHistory.length > masterMind[i].feedbackHistory.length){
+                    
+                    //must wait, feedback not yet returned
+                    revert("error, must wait feedback not yet returned");//non sono sicuro se qui il revert vada bene come error handling
+                }else{
+
+                    revert("internal error");
+                }
             }
         }
         revert("Game not found: no active game for this ID");
@@ -124,11 +164,27 @@ contract game {
     function giveFeedback(uint matchID, Feedback memory uploadedFeedback) public onlyCodeMaker(matchID){
         //uncheck the afk flag 
         actOnAfkFlag(matchID,false);//true is set, false is unset
+
+        //checkFeeback
+        require(checkFeeback(uploadedFeedback),"incorrectly set feedback, ivalid value");
+
         //upload the guess in the array of moves
         for (uint i = 0; i < masterMind.length; i++) {
             if (masterMind[i].matchID == matchID) {
-                masterMind[i].feedbackHistory.push(uploadedFeedback); 
-                emit DepositFeedback(msg.sender,uploadedFeedback,matchID);
+                if (masterMind[i].movesHistory.length > masterMind[i].feedbackHistory.length){
+
+                    masterMind[i].feedbackHistory.push(uploadedFeedback); 
+                    emit DepositFeedback(msg.sender,uploadedFeedback,matchID);
+                }else if (masterMind[i].movesHistory.length == masterMind[i].feedbackHistory.length){
+                    
+                    //must wait, feedback not yet returned
+                    revert("error, must wait guess not yet made");//non sono sicuro se qui il revert vada bene come error handling
+                }else{
+
+                    revert("internal error");
+                }
+
+                
             }
         }
         revert("Game not found: no active game for this ID");
