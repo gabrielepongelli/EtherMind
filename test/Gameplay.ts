e269cbeb7d6c8b6aaaa33379983b13592be86388
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-06-28 12:04:30 +0200

    Added tests for the guess submission.

--- test/Gameplay.ts
+++ test/Gameplay.ts
@@ -112,9 +112,110 @@ describe("Gameplay", function () {
 
     describe("Guess submission", function () {
         describe("Validation", function () {
+            it("Should fail if an invalid match ID is passed", async function () {
+                const { game, codeBreaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const invalidMatchId = ethers.ZeroAddress;
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeBreaker).newGuess(invalidMatchId, guess)).to.be.revertedWith("The match specified does not exist");
+            });
+
+            it("Should fail if called on a match that is in the wrong phase", async function () {
+                const guess = newCode(1, 2, 3, 4);
+
+                for (const phaseFn of Object.values(phases)) {
+                    if (phaseFn == phases.untilFirstRoundCodeHash
+                        || phaseFn == phases.untilFirstRoundFirstFeedback
+                        || phaseFn == phases.untilFirstRoundSecondFeedback
+                        || phaseFn == phases.untilSecondRoundCodeHash) {
+                        return;
+                    }
+
+                    const { game, challenger, matchId } = await loadFixture(phaseFn);
+
+                    await expect(game.newGuess(matchId, guess)).to.be.revertedWith("Operation not permitted in this phase of the game");
+                    await expect(game.connect(challenger).newGuess(matchId, guess)).to.be.revertedWith("Operation not permitted in this phase of the game");
+                }
+            });
+
+            it("Should fail if called by someone which is not part of the match", async function () {
+                const { game, matchId, otherPlayer } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(otherPlayer).newGuess(matchId, guess)).to.be.revertedWith("The caller is not the CodeBreaker");
+            });
+
+            it("Should fail if called from the CodeMaker", async function () {
+                const { game, matchId, codeMaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeMaker).newGuess(matchId, guess)).to.be.revertedWith("The caller is not the CodeBreaker");
+            });
+
+            it("Should fail if called more than once consecutively by the CodeBreaker", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(1, 2, 3, 4);
+
+                await game.connect(codeBreaker).newGuess(matchId, guess);
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).to.be.revertedWith("Operation not permitted in this phase of the game");
+            });
+
+            it("Should fail if called with an invalid code format", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(7, 7, 7, 7);
+
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).to.be.revertedWith("Invalid guess format");
+            });
+
+            it("Should fail if called after the submission of a feedback regarding a correct guess", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilSecondRoundFeedback);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).to.be.revertedWith("Operation not permitted in this phase of the game");
+            });
+
+            it("Should fail if called after the maximum number of guesses is reached", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundLastFeedback);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).to.be.revertedWith("Operation not permitted in this phase of the game");
+            });
+
+            it("Should not fail if called from the CodeBreaker after the submission of the hashed solution", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).not.to.be.reverted;
+            });
+
+            it("Should not fail if called from the CodeBreaker after the submission of a feedback regarding a wrong guess", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundFirstFeedback);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).not.to.be.reverted;
+            });
         });
 
         describe("Events", function () {
+            it("Should emit an event when called by the CodeBreaker", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(1, 2, 3, 4);
+
+                await expect(game.connect(codeBreaker).newGuess(matchId, guess)).to.emit(game, "GuessSubmitted");
+            });
+
+            it("Should emit an event when called by the CodeBreaker with valid parameters", async function () {
+                const { game, matchId, codeBreaker } = await loadFixture(phases.untilFirstRoundCodeHash);
+                const guess = newCode(1, 2, 3, 4);
+
+                const tx = await game.connect(codeBreaker).newGuess(matchId, guess);
+                const eventInterface = new ethers.Interface(["event GuessSubmitted(address id, address from, uint16 guess)"]);
+                const event = await getEvent(tx, eventInterface, "GuessSubmitted");
+
+                expect(event.id).to.be.equal(matchId);
+                expect(event.from).to.be.equal(codeBreaker.address);
+                expect(event.guess).to.be.equal(guess);
+            });
         });
     });
 
