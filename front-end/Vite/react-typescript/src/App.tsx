996bb42591701c4406b7fc7fb5f6a30aa5f01520
jacopo1498 <jacopo1498@gmail.com>
2024-06-28 20:36:18 +0200

    WIP, building general structure of frontend

--- front-end/Vite/react-typescript/src/App.tsx
+++ front-end/Vite/react-typescript/src/App.tsx
@@ -3,11 +3,11 @@
 import React, { useState } from 'react';
 import { GuessForm } from './component/GuessForm';
 
-enum phase { start, match_waiting, round_play_guesser, round_play_master, round_end_master, round_end_guesser, game_end }
+enum phase { start, match_waiting, decide_stake, round_play_guesser, round_play_master, round_end_master, round_end_guesser, game_end }
 //just to keep track of the state of the game
 
 
-let game_state = phase.round_play_guesser;
+let game_state = phase.start;
 
 const App: React.FC = () => {
   const [guesses, setGuesses] = useState<number[][]>([]);
@@ -18,12 +18,17 @@ const App: React.FC = () => {
   };
 
   const [inputValue1, setInputValue1] = useState<string>('');
+  const [inputValue12, setInputValue12] = useState<string>('');
   const [inputValue2, setInputValue2] = useState<string>('');
+  const [inputStake, setInputStake] = useState<string>('');
+
 
   const handleSubmit1 = () => {
     console.log('join Submitted:', inputValue1);
+    console.log('stake Submitted:', inputValue12);
     //TODO call the contract join function from here
     setInputValue1(''); // Reset the input field after submission
+    setInputValue12(''); // Reset the input field after submission
   };
 
   const handleSubmit2 = () => {
@@ -31,6 +36,11 @@ const App: React.FC = () => {
     //TODO call the contract create function from here
     game_state = phase.match_waiting;
     setInputValue2(''); // Reset the input field after submission
+  };
+
+  const handleStake = () => {
+    console.log('new stake Submitted:', inputStake);
+    setInputStake(''); // Reset the input field after submission
   };
 
 
@@ -44,17 +54,24 @@ if(game_state == phase.start)
       <div>
         <h2>Join</h2>
         <input
-          type="text"
+          type="number"
           value={inputValue1}
           onChange={(e) => setInputValue1(e.target.value)}
           placeholder="Enter machID or 0"
         />
+        <input
+          type="number"
+          value={inputValue12}
+          onChange={(e) => setInputValue12(e.target.value)}
+          placeholder="Enter initial stake"
+        />
         <button onClick={handleSubmit1}>join match</button>
+        <h3>{inputValue1}</h3>
       </div>
       <div>
         <h2>Create</h2>
         <input
-          type="text"
+          type="number"
           value={inputValue2}
           onChange={(e) => setInputValue2(e.target.value)}
           placeholder="Enter machID or 0"
@@ -71,6 +88,22 @@ if(game_state == phase.start)
       </div>
     );
 
+  }else if(game_state == phase.decide_stake){
+    return(//TO DO ADD A WAY TO DISPLAY CURRENT STAKE WHEN AGREE MOVE TO NEXT STEP
+      <div>
+      <h2>Agree on stake of the game</h2>
+      <input
+        type="number"
+        value={inputStake}
+        onChange={(e) => setInputStake(e.target.value)}
+        placeholder="Enter stake"
+      />
+      <button onClick={handleStake}>Submit</button>
+      <div>
+        <h3>current stake</h3>
+      </div>
+    </div>
+    );
   }else if(game_state == phase.round_play_guesser){
 
     return (
