37a170ce49d844d1df109e65dc56b2c3c962928e
jacopo1498 <jacopo1498@gmail.com>
2024-06-30 21:46:42 +0200

    WIP, fixed hardhat config  NEEDS TESTING

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter";
 
+//get api key and private key from cli
+const { vars } = require("hardhat/config");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
+
+
 const config: HardhatUserConfig = {
     solidity: {
         version: "0.8.24",
@@ -17,7 +24,17 @@ const config: HardhatUserConfig = {
     gasReporter: {
         currency: 'EUR',
         coinmarketcap: process.env.COINMARKETCAP_API_KEY
-    }
+    },
+    networks: {
+        sepolia: {
+        url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+        accounts: [SEPOLIA_PRIVATE_KEY],
+        },
+    },
 };
 
 export default config;
+
+
+
+
