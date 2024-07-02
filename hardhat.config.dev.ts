312163aad971ba547a27d6eb8a55f94d56e4d43a
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-07-02 12:13:49 +0200

    Splitted dev and prod configs.

---
+++ hardhat.config.dev.ts
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.24",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    gasReporter: {
+        currency: 'EUR',
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY
+    }
+};
+
+export default config;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,40 +1,5 @@
 require('dotenv').config();
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-gas-reporter";
+const configFile = `./hardhat.config.${process.env.HARDHAT_CONFIG || 'dev'}.ts`;
 
-//get api key and private key from cli
-const { vars } = require("hardhat/config");
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
-
-
-
-const config: HardhatUserConfig = {
-    solidity: {
-        version: "0.8.24",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
-        },
-    },
-    gasReporter: {
-        currency: 'EUR',
-        coinmarketcap: process.env.COINMARKETCAP_API_KEY
-    },
-    networks: {
-        sepolia: {
-        url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-        accounts: [SEPOLIA_PRIVATE_KEY],
-        },
-    },
-};
-
-export default config;
-
-
-
-
+module.exports = require(configFile);
