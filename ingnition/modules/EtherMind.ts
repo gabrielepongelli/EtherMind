36d1fec17424ee544fe04f5c66fe6db5a9063b90
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-07-02 11:51:24 +0200

    Modified ignition module.

--- ingnition/modules/EtherMind.ts
+++ ingnition/modules/EtherMind.ts
@@ -1,15 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const ONE_GWEI: bigint = 1_000_000_000n;
-
 const EMModule = buildModule("EMModule", (m) => {
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-
-  //NO CONSTRUCTOR? WHAT NOW?
-  const EM = m.contract("EtherMind");
-
-  return { EM };
+    const EM = m.contract("EtherMind");
+    return { EM };
 });
 
 export default EMModule;
