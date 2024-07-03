891b3a0110cd2de87b0855bdcf4230cbfed3e787
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-07-03 19:07:15 +0200

    Deleted unneeded file.

--- ingnition/modules/EtherMind.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const EMModule = buildModule("EMModule", (m) => {
-    const EM = m.contract("EtherMind");
-    return { EM };
-});
-
-export default EMModule;
