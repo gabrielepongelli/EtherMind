9f9f67490f57980468312a2e2c1344268d16c3ac
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-07-04 15:13:13 +0200

    Created button component.

---
+++ frontend/src/component/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface ButtonProps {
+    text: string; // The text to display inside the button
+    danger: boolean; // Whether this is a button representing a dangerous operation
+    onclick: React.MouseEventHandler<HTMLButtonElement>; // Callback on button click
+    disabled: boolean; // Whether the button can be interacted with
+}
+
+export const Button: React.FC<ButtonProps> = ({ text, danger, onclick, disabled }) => {
+    return (
+        <button
+            type="button"
+            className={danger ? "btn btn-danger container" : "btn btn-primary container"}
+            onClick={onclick}
+            disabled={disabled}
+        >
+            <span className='fw-bold'>
+                {text}
+            </span>
+        </button>
+    );
+}
